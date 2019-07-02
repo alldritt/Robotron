efc2f743209dec453cd44cd7113d283c2e094e15
Mark Alldritt <alldritt@latenightsw.com>
2019-07-01 19:28:31 -0700

    Add GameController framework support to the macOS build.

--- Robotron Mac/AppDelegate.m
+++ Robotron Mac/AppDelegate.m
@@ -9,12 +9,14 @@
 #import "AppDelegate.h"
 #import "MyScene.h"
 #import <DDHidLib/DDHidLib.h>
+#import <GameController/GameController.h>
 
 
 @interface AppDelegate ()
 
 @property (strong, nonatomic) MyScene* gameScene;
 @property (strong, nonatomic) NSArray* joysticks;
+@property (strong, nonatomic) GCController* controller;
 
 @end
 
@@ -25,7 +27,7 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     /* Pick a size for the scene */
-    self.gameScene = [MyScene sceneWithSize:CGSizeMake(1024, 768)];
+    self.gameScene = [[MyScene alloc] initWithSize:CGSizeMake(1024, 768) withVisualController:FALSE];
 
     /* Set the scale mode to scale to fit the window */
     self.gameScene.scaleMode = SKSceneScaleModeAspectFit;
@@ -35,7 +37,31 @@
     // Configure the view.
     self.skView.showsFPS = YES;
     self.skView.showsNodeCount = YES;
+
+    [[NSNotificationCenter defaultCenter] addObserverForName:GCControllerDidConnectNotification
+                                                      object:nil
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification* note) {
+                                                      GCController* controller = (GCController*) note.object;
+                                                      GCExtendedGamepad* gamepad = controller.extendedGamepad;
+                                                      
+                                                      if (gamepad)
+                                                          [self connectToController:controller];
+                                                  }];
+    [[NSNotificationCenter defaultCenter] addObserverForName:GCControllerDidDisconnectNotification
+                                                      object:nil
+                                                       queue:nil
+                                                  usingBlock:^(NSNotification* note) {
+                                                      GCController* controller = (GCController*) note.object;
+                                                      
+                                                      if (self.controller == controller)
+                                                          [self disconnectController:controller];
+                                                  }];
     
+    [GCController startWirelessControllerDiscoveryWithCompletionHandler:^{
+        NSLog(@"controllers: %@", [GCController controllers]);
+    }];
+
     self.joysticks = [DDHidJoystick allJoysticks];
     [self.joysticks makeObjectsPerformSelector:@selector(setDelegate:) withObject:self];
     [self.joysticks makeObjectsPerformSelector:@selector(startListening) withObject:nil];
@@ -48,6 +74,7 @@
 - (void) ddhidJoystick: (DDHidJoystick *) joystick
                  stick: (unsigned) stick
               xChanged: (int) value {
+    if (self.controller) return;
     value = value / 16000;
     
     if (value == 0)
@@ -61,6 +88,7 @@
 - (void) ddhidJoystick: (DDHidJoystick *) joystick
                  stick: (unsigned) stick
               yChanged: (int) value {
+    if (self.controller) return;
     value = value / 16000;
     
     if (value == 0)
@@ -75,6 +103,7 @@
                  stick: (unsigned) stick
              otherAxis: (unsigned) otherAxis
           valueChanged: (int) value {
+    if (self.controller) return;
     value = value / 16000;
 
     if (otherAxis == 0) {
@@ -99,11 +128,13 @@
                  stick: (unsigned) stick
              povNumber: (unsigned) povNumber
           valueChanged: (int) value {
+    if (self.controller) return;
     //NSLog(@"ddhidJoystick: %@ stick: %d povNumber: %d valueChanged: %d", joystick, stick, povNumber, value);
 }
 
 - (void) ddhidJoystick: (DDHidJoystick *) joystick
             buttonDown: (unsigned) buttonNumber {
+    if (self.controller) return;
     //NSLog(@"ddhidJoystick: %@ buttonDown: %d", joystick, buttonNumber);
     //  On my Logitech Dual Action game controller, buttons 10 and 11 happen when either of the joysticks
     //  are pressed down which happens accepdently too often, so I'm disabling them here.
@@ -115,7 +146,60 @@
 
 - (void) ddhidJoystick: (DDHidJoystick *) joystick
               buttonUp: (unsigned) buttonNumber {
+    if (self.controller) return;
     //NSLog(@"ddhidJoystick: %@ buttonUp: %d", joystick, buttonNumber);
+}
+
+//  MARK: GCController callbacks
+
+- (void) connectToController:(GCController*) controller {
+    if (self.controller) {
+        [self disconnectController:self.controller];
+    }
+    
+    self.controller = controller;
+    
+    controller.extendedGamepad.leftThumbstick.valueChangedHandler = ^(GCControllerDirectionPad * dpad, float xValue, float yValue) {
+        //  Move stick
+        self.gameScene.moveX = ABS(xValue) <= 0.15 ? 0.0 : (xValue > 0.0 ? 1.0 : -1.0);
+        self.gameScene.moveY = ABS(yValue) <= 0.15 ? 0.0 : (yValue > 0.0 ? 1.0 : -1.0);
+    };
+    controller.extendedGamepad.rightThumbstick.valueChangedHandler = ^(GCControllerDirectionPad * dpad, float xValue, float yValue) {
+        //  Fire stick
+        self.gameScene.fireX = ABS(xValue) <= 0.15 ? 0.0 : (xValue > 0.0 ? 1.0 : -1.0);
+        self.gameScene.fireY = ABS(yValue) <= 0.15 ? 0.0 : (yValue > 0.0 ? 1.0 : -1.0);
+    };
+    controller.extendedGamepad.buttonX.pressedChangedHandler = ^(GCControllerButtonInput* button, float value, BOOL pressed) {
+        if (pressed) {
+            [self.gameScene togglePaused];
+        }
+    };
+    controller.extendedGamepad.buttonY.pressedChangedHandler = ^(GCControllerButtonInput* button, float value, BOOL pressed) {
+        if (pressed) {
+            [self.gameScene togglePaused];
+        }
+    };
+    controller.extendedGamepad.buttonA.pressedChangedHandler = ^(GCControllerButtonInput* button, float value, BOOL pressed) {
+        if (pressed) {
+            [self.gameScene togglePaused];
+        }
+    };
+    controller.extendedGamepad.buttonB.pressedChangedHandler = ^(GCControllerButtonInput* button, float value, BOOL pressed) {
+        if (pressed) {
+            [self.gameScene togglePaused];
+        }
+    };
+}
+
+- (void) disconnectController:(GCController*) controller {
+    self.controller.extendedGamepad.leftThumbstick.valueChangedHandler = nil;
+    self.controller.extendedGamepad.rightThumbstick.valueChangedHandler = nil;
+    self.controller.extendedGamepad.buttonX.pressedChangedHandler = nil;
+    self.controller.extendedGamepad.buttonY.pressedChangedHandler = nil;
+    self.controller.extendedGamepad.buttonA.pressedChangedHandler = nil;
+    self.controller.extendedGamepad.buttonB.pressedChangedHandler = nil;
+    self.controller = nil;
+    
 }
 
 @end
